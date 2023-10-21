41ef5e8d32218e7b47d49a71e225d816d4248749
Sergey Aleshin <saleshin@cbr12s2.42barcelona.com>
2023-10-21 12:52:49 +0200

    start to work with file

--- get_next_line.c
+++ get_next_line.c
@@ -9,8 +9,31 @@
 /*   Updated: 2023/08/14 18:34:19 by saleshin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 10
+#endif
 
+int	main(void)
+{
+	char	str[BUFFER_SIZE];
+	char	*new_res;
+	int		fd;
+
+//	str = NULL;
+	fd = open("./test.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		printf("error opening file\n");
+		return (1);
+	}
+	read(fd, str, BUFFER_SIZE);
+	printf("%s\n", str);
+	return (0);
+}
+/*
 void	ft_putstr_fd(char *s, int fd)
 {
 	while (*s != '\0')
@@ -19,3 +42,4 @@ void	ft_putstr_fd(char *s, int fd)
 		s++;
 	}
 }
+*/
